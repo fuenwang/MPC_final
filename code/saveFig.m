a8074fcd523fd35cd4d90e45fa357f23956ef58c
fuenwang <fulton84717@gmail.com>
2018-06-15 17:38:35 +0800

    gg

---
+++ code/saveFig.m
@@ -0,0 +1,7 @@
+function saveFig(fig, path)
+    fig.PaperPositionMode = 'auto';
+    fig_pos = fig.PaperPosition;
+    fig.PaperSize = [fig_pos(3) fig_pos(4)];
+    print(fig, path, '-dpdf')
+end
+
